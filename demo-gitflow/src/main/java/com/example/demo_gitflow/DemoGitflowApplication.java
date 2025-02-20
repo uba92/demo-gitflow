19f18122cd453d22031cdc0301c6a323d6d3c4e2
Umberto Tramontano <u.tramontano92@libero.it>
2025-02-20 12:46:22 +0100

    Initial commit

---
+++ demo-gitflow/src/main/java/com/example/demo_gitflow/DemoGitflowApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_gitflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGitflowApplication.class, args);
+	}
+
+}
---
+++ demo-gitflow/src/test/java/com/example/demo_gitflow/DemoGitflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_gitflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
